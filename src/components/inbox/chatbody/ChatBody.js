8ef89d915a82fe185408dd8b15873b1a462d6892
dev-ahmedali <ahmedali69325@gmail.com>
2023-03-12 15:08:44 +0600

    messages func

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -7,40 +7,46 @@ import Error from "../../ui/Error";
 // import Blank from "./Blank";
 
 export default function ChatBody() {
-    const {id} = useParams()
-    const {data: messages, isLoading, isError, error} = useGetMessagesQuery(id);
+  const { id } = useParams();
+  const { data: messages, isLoading, isError, error } = useGetMessagesQuery(id);
 
-    // decide what to render
-    let content = null;
+  // decide what to render
+  let content = null;
 
-    if(isLoading) {
-        content = <div>Loading...</div>
-    } else if(!isLoading && isError) {
-        content = <div><Error messages={error?.data}/></div>
-    } else if(!isLoading && !isError && messages?.length === 0) {
-        content = <div>No messages found</div>
-    } else if(!isLoading && !isError && messages?.length > 0) {
-        content = <>
-         <ChatHead
-                    avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                    name="Akash Ahmed"
-                />
-                <Messages messages={messages}/>
-                <Options />
-        </>
-    }
-
-    return (
-        <div className="w-full lg:col-span-2 lg:block">
-            <div className="w-full grid conversation-row-grid">
-                <ChatHead
-                    avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-                    name="Akash Ahmed"
-                />
-                <Messages />
-                <Options />
-                {/* <Blank /> */}
-            </div>
-        </div>
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (!isLoading && isError) {
+    content = (
+      <div>
+        <Error messages={error?.data} />
+      </div>
     );
+  } else if (!isLoading && !isError && messages?.length === 0) {
+    content = <div>No messages found</div>;
+  } else if (!isLoading && !isError && messages?.length > 0) {
+    content = (
+      <>
+        <ChatHead
+          avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
+          name="Akash Ahmed"
+        />
+        <Messages messages={messages} />
+        <Options />
+      </>
+    );
+  }
+
+  return (
+    <div className="w-full lg:col-span-2 lg:block">
+      <div className="w-full grid conversation-row-grid">
+        <ChatHead
+          avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
+          name="Akash Ahmed"
+        />
+        <Messages />
+        <Options />
+        {/* <Blank /> */}
+      </div>
+    </div>
+  );
 }
