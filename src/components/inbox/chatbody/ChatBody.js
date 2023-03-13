4a1ab52c708a852a28c2c862adb57546471ba41a
dev-ahmedali <ahmedali69325@gmail.com>
2023-03-13 14:54:17 +0600

    added messages func2

--- src/components/inbox/chatbody/ChatBody.js
+++ src/components/inbox/chatbody/ChatBody.js
@@ -10,6 +10,8 @@ export default function ChatBody() {
   const { id } = useParams();
   const { data: messages, isLoading, isError, error } = useGetMessagesQuery(id);
 
+  console.log(messages);
+
   // decide what to render
   let content = null;
 
@@ -18,7 +20,7 @@ export default function ChatBody() {
   } else if (!isLoading && isError) {
     content = (
       <div>
-        <Error messages={error?.data} />
+        <Error message={error?.data} />
       </div>
     );
   } else if (!isLoading && !isError && messages?.length === 0) {
@@ -39,13 +41,7 @@ export default function ChatBody() {
   return (
     <div className="w-full lg:col-span-2 lg:block">
       <div className="w-full grid conversation-row-grid">
-        <ChatHead
-          avatar="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg"
-          name="Akash Ahmed"
-        />
-        <Messages />
-        <Options />
-        {/* <Blank /> */}
+       {content}
       </div>
     </div>
   );
--- src/components/inbox/chatbody/Messages.js
+++ src/components/inbox/chatbody/Messages.js
@@ -2,15 +2,20 @@ import { useSelector } from "react-redux";
 import Message from "./Message";
 
 export default function Messages({ messages = [] }) {
-    const {user} = useSelector(state => state.auth) || {}
-    const {email} = user || {}
+  const { user } = useSelector((state) => state.auth) || {};
+  const { email } = user || {};
+
   return (
     <div className="relative w-full h-[calc(100vh_-_197px)] p-6 overflow-y-auto flex flex-col-reverse">
       <ul className="space-y-2">
-        {messages.map((message) => {
-          const { message: lastMessage, id } = message || {};
-          return <Message key={id} justify="start" message={lastMessage} />;
-        })}
+        {messages
+         .slice()
+          .sort((a, b) => a.timpstamp - b.timestamp)
+          .map((message) => {
+            const { message: lastMessage, id, sender } = message || {};
+            const justify = sender.email !== email ? "start" : "end";
+            return <Message key={id} justify={justify} message={lastMessage} />;
+          })}
       </ul>
     </div>
   );
